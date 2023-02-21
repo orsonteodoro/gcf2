b30e25b7ac168d5c12747a58d84093b331fd19e8
Orson Teodoro <orsonteodoro@hotmail.com>
2023-02-20 17:50:17 -0800

    Add updated -fuse-ld=mold patch in gaming profile

--- profiles/gaming/README.md
+++ profiles/gaming/README.md
@@ -13,7 +13,7 @@ lag or annoying glitches that that will produce unfair play.
 * &gt;= 57 FPS for high motion gaming with newer games.
 * The fast install is a necessary requirement to maximize play time over build
 time, but at the same time the gameplay should not be unfair.
-* GCC &ge; 12 for mold, or GCC 11 with [mold patch](https://wiki.gentoo.org/wiki/Mold#GCC_11).
+* GCC &ge; 12 for mold, or GCC 11 with [mold patch](../patches/sys-devel/gcc:11/0000-gcc-11.3.1_p20230120-r1-ld.mold-support.patch).
 * Stable profile and stable keywords is preferred
 
 ## Tradeoffs / performance
--- profiles/gaming/install.sh
+++ profiles/gaming/install.sh
@@ -10,6 +10,7 @@ main() {
 	cp -a "${dest}/make.conf"{,.${ts}}
 	cp -a "make.conf" "${dest}"
 	cp -a "env" "${dest}"
+	cp -a "patches" "${dest}"
 	chown root:root "${dest}/make.conf"
 	chown root:root "${dest}/package.env"
 	chown -R root:root "${dest}/env"
@@ -17,6 +18,17 @@ main() {
 	chmod 644 "${dest}/package.env"
 	chmod -R 644 "${dest}/env/"*
 	chmod -R 755 "${dest}/env"
+
+	# We do it this way assuming sensitive data (credentials) are used as a patch.
+	chown root:root "${dest}/patches/sys-devel/gcc:11/0000-gcc-11.3.1_p20230120-r1-ld.mold-support.patch"
+	chown root:root "${dest}/patches/sys-devel/gcc:11"
+	chown root:root "${dest}/patches/sys-devel"
+	chown root:root "${dest}/patches"
+	chmod 644 "${dest}/patches/sys-devel/gcc:11/0000-gcc-11.3.1_p20230120-r1-ld.mold-support.patch"
+	chmod 755 "${dest}/patches/sys-devel/gcc:11"
+	chmod 755 "${dest}/patches/sys-devel"
+	chmod 755 "${dest}/patches"
+
 echo
 echo "You need to manually update ${dest}/make.conf from"
 echo "${dest}/make.conf.${ts}"
